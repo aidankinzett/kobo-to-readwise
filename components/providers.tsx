1588eb43e6776a6680304285770e090fca72a621
Aidan Kinzett <aidan@kinzett.io>
2024-12-14 22:17:32 +1000

    fix: fix update checker

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { checkForUpdates } from "@/lib/updates";
+import { useUpdateChecker } from "@/hooks/use-update-checker";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, useEffect, useState } from "react";
 
 export function Providers({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
 
+  const { checkForUpdates } = useUpdateChecker();
+
   useEffect(() => {
     checkForUpdates();
-  }, []);
+  }, [checkForUpdates]);
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
